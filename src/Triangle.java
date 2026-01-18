71387c7ae785d86e7ad7e6854c0c8b57bedf4f54
Alwin Sunu <alwinsunu1974@gmail.com>
2026-01-18 22:00:45 +0530

    Change variable name from 'rows' to 'height'

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,8 +1,8 @@
 public class Triangle {
     public static void main(String args[]) {
-        int rows = 3;
+        int height = 3;
 
-        for (int i = 0; i < rows; i++) {
+        for (int i = 0; i < height; i++) {
             for (int j = 0; j < (2 * i + 1); j++) {
                 System.out.print("* ");
             }
