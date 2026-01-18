265f372480568a3929dbfb4a73e8420c61eb2314
Alwin Sunu <alwinsunu1974@gmail.com>
2026-01-18 21:39:14 +0530

    Add main method to Triangle class
    Implement main method to print a triangle pattern.

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,3 +1,12 @@
 public class Triangle {
     //Add your programs here
+    public static void main(String args[]){
+        int base=5;
+        int height=3;
+        for(int i=0;i<height;i++){
+            for(int i=1;i<base;i+2){
+                System.out.println("* ");
+            }
+        }
+    }
 }
