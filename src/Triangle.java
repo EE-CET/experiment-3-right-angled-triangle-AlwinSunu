e993889055205e207e084d387ea83349f9e8e82b
Alwin Sunu <alwinsunu1974@gmail.com>
2026-01-31 13:51:51 +0530

    Refactor main method in Triangle class

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,10 +1,14 @@
 public class Triangle {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         int height = 3;
 
         for (int i = 0; i < height; i++) {
-            for (int j = 0; j < (2 * i + 1); j++) {
-                System.out.print("* ");
+            int num = 2 * i + 1;
+            for (int j = 0; j < num; j++) {
+                System.out.print("*");
+                if (j < num - 1) {
+                    System.out.print(" ");
+                }
             }
             System.out.println();
         }
